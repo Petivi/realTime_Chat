59280ad3b9570cede52cd2680daad2b2416c44c2
Neskwik93 <eric.bauman@laposte.net>
2019-06-17 23:03:23 +0200

    pseudo

--- chat/server.js
+++ chat/server.js
@@ -4,11 +4,17 @@ const socketio = require('socket.io');
 
 var app = express();
 app.use(express.static('static'));
-var server = app.listen(3000);
+var server = app.listen(3000, () => {
+    console.log('serveur ecoutant sur le port 3000...')
+});
 var io = socketio(server);
 
 io.on('connection', client => {
+    client.on('chosePseudo', data => {
+        client.pseudo = data;
+    });
+
     client.on('sendMessage', data => {
-        io.emit('event', data);
+        io.emit('event', { pseudo: client.pseudo, text: data });
     });
 });
