d8298f8631884b0a1f397598d02e34306ebb890a
Neskwik93 <eric.bauman@laposte.net>
2019-06-18 12:33:46 +0200

    .

--- chat/server.js
+++ chat/server.js
@@ -9,18 +9,28 @@ var server = app.listen(3000, () => {
 });
 var io = socketio(server);
 
+var ttRoom = [];
+
 io.on('connection', client => {
     client.on('connectToRoom', data => {
         client.pseudo = data.pseudo;
         client.room = data.room;
+        let room = ttRoom.find(room => room.name === data.room)
+        if (room) { //si la salle existe déjà on ajout juste le mec dedans
+            room.users.push(data.pseudo);
+        } else {
+            room = { name: data.room, users: [data.pseudo] }
+            ttRoom.push(room);
+        }
 
         client.join(client.room); // on rejoint / créé la room choisie
         io.to(client.id).emit('roomJoin', { room: client.room, text: "You joined the room !" }); // affichage pour le sender
-        client.to(client.room).emit('userJoin', {text:client.pseudo + " joined the room !"}); // affichage pour les autres
+        client.to(client.room).emit('userJoin', { text: client.pseudo + " joined the room !" }); // affichage pour les autres
+        io.to(client.room).emit('listUser', room.users);
     });
 
     client.on('sendMessage', data => {
-        io.to(client.id).emit('event', { pseudo: '<span class="colorSender">'+client.pseudo+'</span>', text: data }); // affichage pour le sender
+        io.to(client.id).emit('event', { pseudo: '<span class="colorSender">' + client.pseudo + '</span>', text: data }); // affichage pour le sender
         client.to(client.room).emit('event', { pseudo: client.pseudo, text: data }); // affichage pour les autres
     });
 });
